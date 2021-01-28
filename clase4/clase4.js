d879aa61c8a62bae2612b114f7e8e762181238a4
Anderson David Patino Caicedo <andersondavidpc@ufps.edu.co>
2021-01-27 23:58:59 -0500

    Clase 4

---
+++ clase4/clase4.js
@@ -0,0 +1,10 @@
+// var nombre='Anderson', edad=19;
+// function imprimirEdad() {
+//   console.log(`${nombre} tiene ${edad} años`);
+// }
+// imprimirEdad();
+
+function imprimirEdad(nombre, edad) {
+  console.log(`${nombre} tiene ${edad} años`);
+}
+imprimirEdad('Anderson', 19);
