1c52b71a831154cb5020c6d7953108599c1d26a5
Anderson David Patino Caicedo <andersondavidpc@ufps.edu.co>
2021-01-27 22:52:32 -0500

    Clase 2

---
+++ clase2/clase2.js
@@ -0,0 +1,10 @@
+var nombre='Anderson', apellido='Patiño';
+var nombreMayus=nombre.toUpperCase();
+var apellidoMinus=apellido.toLowerCase();
+var primeraLetraNombre=nombre.charAt(0);
+var cantidadLetrasNombre=nombre.length;
+var nombreCompleto=nombre+' '+apellido;
+var nombreCompleto2=`${nombre} ${apellido.toUpperCase()}`;
+
+var str=nombre.substr(1,2);
+var ultLetraNombre=nombre.charAt(nombre.length-1);
